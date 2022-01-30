b6aab47dd972de7dccfc8ac299cf37aaab47efae
adnanrahin <adnan.rahin13@gmail.com>
2022-01-29 17:55:38 -0800

    Airflow task updated

--- sensor_part_one.py
+++ sensor_part_one.py
@@ -1,5 +1,6 @@
 import airflow.utils.dates
 from airflow import DAG
+from airflow.sensors.filesystem import FileSensor
 from airflow.operators.dummy import DummyOperator
 
 dag = DAG(
@@ -13,6 +14,11 @@ dag = DAG(
 create_metrics = DummyOperator(task_id="create_metrics", dag=dag)
 
 for supermarket_id in [1, 2, 3, 4]:
+    wait = FileSensor(
+        task_id=f"wait_for_supermarket_{supermarket_id}",
+        filepath=f"/data/supermarket{supermarket_id}/data.csv",
+        dag=dag,
+    )
     copy = DummyOperator(task_id=f"copy_to_raw_supermarket_{supermarket_id}", dag=dag)
     process = DummyOperator(task_id=f"process_supermarket_{supermarket_id}", dag=dag)
-    copy >> process >> create_metrics
+    wait >> copy >> process >> create_metrics
