b2638551fe173353d369506446c4fd1c44c3adbb
adnanrahin <adnan.rahin13@gmail.com>
2022-02-01 20:55:18 -0800

    dags update

--- spark_flight_data_dags.py
+++ spark_flight_data_dags.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import pandas as pd
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
 from airflow.sensors.filesystem import FileSensor
 from airflow.operators.python import PythonOperator
 
@@ -19,26 +20,43 @@ build_jar = BashOperator(
     dag=dag,
 )
 
-wait_for_jar_to_build = FileSensor(
-    task_id='wait_for_jar_to_build',
-    file_path='~/source-code/Intellij-Project/Spark-Flights-Data-Analysis/target/'
-              'spark-flights-data-analysis-1.0-SNAPSHOT.jar'
+# wait_for_jar_to_build = FileSensor(
+#     task_id='wait_for_jar_to_build',
+#     file_path='~/source-code/Intellij-Project/Spark-Flights-Data-Analysis/target/'
+#               'spark-flights-data-analysis-1.0-SNAPSHOT.jar',
+#     dag=dag
+# )
+#
+# jar_join_dummy_task = DummyOperator(
+#     task_id='jar_join_dummy_task',
+#     dag=dag,
+# )
+
+
+def run_all_spark_job():
+    for job_id in range(1, 7):
+        submit_spark_job = BashOperator(
+            task_id=f'submit_spark_job_{job_id}',
+            bash_command=f'spark-submit --class org.flight.analysis.FlightDataProcessor '
+                         '--master spark://ubuntu:7077 '
+                         '--deploy-mode cluster '
+                         '--executor-memory 16G '
+                         '--total-executor-cores 12 '
+                         '--driver-memory 16G '
+                         '--driver-cores 12 '
+                         '~/source-code/Intellij-Project/Spark-Flights-Data-Analysis/target/spark-flights'
+                         '-data-analysis-1.0-SNAPSHOT.jar '
+                         '~/source-code/Intellij-Project/Spark-Flights-Data-Analysis/2015_flights_data/ '
+                         f'~/source-code/Intellij-Project/Spark-Flights-Data-Analysis/filter_data {job_id}',
+            dag=dag,
+        )
+        submit_spark_job.execute(dict())
+
+
+spark_job = PythonOperator(
+    task_id='spark_job_runner',
+    python_callable=run_all_spark_job,
+    dag=dag
 )
 
-for job_id in range(1, 7):
-    submit_spark_job = BashOperator(
-        task_id=f'submit_spark_job_{job_id}',
-        bash_command=f'spark-submit --class org.flight.analysis.FlightDataProcessor '
-                     '--master spark://ubuntu:7077 '
-                     '--deploy-mode cluster '
-                     '--executor-memory 16G '
-                     '--total-executor-cores 12 '
-                     '--driver-memory 16G '
-                     '--driver-cores 12 '
-                     '~/source-code/Intellij-Project/Spark-Flights-Data-Analysis/target/spark-flights'
-                     '-data-analysis-1.0-SNAPSHOT.jar '
-                     '~/source-code/Intellij-Project/Spark-Flights-Data-Analysis/2015_flights_data/ '
-                     f'~/source-code/Intellij-Project/Spark-Flights-Data-Analysis/filter_data {job_id}',
-        dag=dag,
-    )
-    build_jar >> submit_spark_job
+build_jar >> spark_job
