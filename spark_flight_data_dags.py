bd82ac47179302877dd2805ef6df7aedef1aaf4b
adnanrahin <adnan.rahin13@gmail.com>
2022-02-01 16:48:19 -0800

    all spark job added to airflow dag

--- spark_flight_data_dags.py
+++ spark_flight_data_dags.py
@@ -18,20 +18,20 @@ build_jar = BashOperator(
     dag=dag,
 )
 
-submit_spark_job = BashOperator(
-    task_id='submit_spark_job',
-    bash_command='spark-submit --class org.flight.analysis.FlightDataProcessor '
-                 '--master spark://ubuntu:7077 '
-                 '--deploy-mode cluster '
-                 '--executor-memory 16G '
-                 '--total-executor-cores 12 '
-                 '--driver-memory 16G '
-                 '--driver-cores 12 '
-                 '~/source-code/Intellij-Project/Spark-Flights-Data-Analysis/target/spark-flights'
-                 '-data-analysis-1.0-SNAPSHOT.jar '
-                 '~/source-code/Intellij-Project/Spark-Flights-Data-Analysis/2015_flights_data/ '
-                 '~/source-code/Intellij-Project/Spark-Flights-Data-Analysis/filter_data 4',
-    dag=dag,
-)
-
-build_jar >> submit_spark_job
+for job_id in range(1, 6):
+    submit_spark_job = BashOperator(
+        task_id=f'submit_spark_job_{job_id}',
+        bash_command=f'spark-submit --class org.flight.analysis.FlightDataProcessor '
+                     '--master spark://ubuntu:7077 '
+                     '--deploy-mode cluster '
+                     '--executor-memory 16G '
+                     '--total-executor-cores 12 '
+                     '--driver-memory 16G '
+                     '--driver-cores 12 '
+                     '~/source-code/Intellij-Project/Spark-Flights-Data-Analysis/target/spark-flights'
+                     '-data-analysis-1.0-SNAPSHOT.jar '
+                     '~/source-code/Intellij-Project/Spark-Flights-Data-Analysis/2015_flights_data/ '
+                     f'~/source-code/Intellij-Project/Spark-Flights-Data-Analysis/filter_data {job_id}',
+        dag=dag,
+    )
+    build_jar >> submit_spark_job
