a8f990173e539bc8968d0a7e0328490b5544f959
adnanrahin <adnan.rahin13@gmail.com>
2022-02-09 21:37:51 -0800

    succes files parent directory isolated

--- file_manager.py
+++ file_manager.py
@@ -5,11 +5,16 @@ from pathlib import Path
 root_file_dir = '/home/rahin/source-code/Intellij-Project/Spark-Flights-Data-Analysis/filter_data/'
 file_list = glob.iglob(root_file_dir + '**/**', recursive=True)
 
-list(
-    map(lambda x: print(x),
-
-        file_list)
+success_files_list = filter(
+    lambda x: (
+        x.endswith('_SUCCESS')
+    )
+    , file_list
 )
+
+success_files_parents = set(map(lambda file: Path(file).parent, success_files_list))
+
+list(map(lambda x: print(x), success_files_parents))
 
 # print(filename)
 # print(Path(filename).parent)
