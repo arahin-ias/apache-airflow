fcd792753e3a10ec96d431293a08320a2dfac636
adnanrahin <adnan.rahin13@gmail.com>
2022-02-21 17:34:08 -0800

    update

--- s3_hooks.py
+++ s3_hooks.py
@@ -1,23 +1,9 @@
-import datetime as dt
-from pathlib import Path
-import logging
-import pandas as pd
-from airflow import DAG
-from airflow.operators.bash import BashOperator
-from airflow.operators.dummy import DummyOperator
-from airflow.providers.amazon.aws.operators.s3 import S3CreateBucketOperator
-from airflow.sensors.filesystem import FileSensor
-from airflow.operators.python import PythonOperator
-import glob
-import os
-import tarfile
-from pathlib import Path
-import boto3
-from botocore.exceptions import ClientError
-from airflow.sensors.python import PythonSensor
+from airflow.hooks.S3_hook import S3Hook
 
-dag = DAG(
-    dag_id='aws_s3_hooks',
-    start_date=dt.datetime(2022, 2, 1),
-    schedule_interval=None,
-)
+
+def create_bucket():
+    hook = S3Hook(aws_conn_id='aws_credentials')
+    hook.create_bucket(bucket_name='optimus-prime-test-arahin')
+
+
+create_bucket()
