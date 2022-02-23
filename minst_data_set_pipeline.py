32c273c5140b48c1cededdf31c0d92cd86eedd66
adnanrahin <adnan.rahin13@gmail.com>
2022-02-22 20:48:02 -0800

    dag created

--- minst_data_set_pipeline.py
+++ minst_data_set_pipeline.py
@@ -1,3 +1,9 @@
 import airflow
 from airflow import DAG
 from airflow.providers.amazon.aws.operators.s3 import S3CreateBucketOperator
+
+dag = DAG(
+    dag_id='aws_handwritten_digit_classifier',
+    schedule_interval=None,
+    start_date=airflow.utils.dates.days_ago(3),
+)
