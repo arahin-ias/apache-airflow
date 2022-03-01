5faddd7c884b0ea3e6ed04b7aa73f02b79109be9
adnanrahin <adnan.rahin13@gmail.com>
2022-02-28 20:39:43 -0800

    update

--- spark_flight_data_dags.py
+++ spark_flight_data_dags.py
@@ -1,13 +1,10 @@
 import datetime as dt
-from pathlib import Path
 import logging
-import pandas as pd
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.amazon.aws.operators.s3 import S3CreateBucketOperator
-from airflow.sensors.filesystem import FileSensor
 from airflow.operators.python import PythonOperator
 import glob
 import os
